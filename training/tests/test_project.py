1f8185cb3cb4446156e077cd100899e334b428db
Katharina Bülow <katharina.buelow@hereon.de>
2024-10-22 18:18:09 +0200

    tests update

--- training/tests/test_project.py
+++ training/tests/test_project.py
@@ -1,7 +1,12 @@
 import pytest
-from .. import add_one
+from .. import add_one, add_two
 
 
 @pytest.mark.parametrize("x, y", [(1, 2), (2, 3), (3, 4)])
 def test_add_one(x, y):
     assert y == add_one(x)
+
+@pytest.mark.parametrize("x, y", [(1, 3), (2, 4), (3, 5)])
+def test_add_two(x, y):
+    assert y == add_two(x)
+   
